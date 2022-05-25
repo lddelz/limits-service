c2056dd3fabf291c4ab5f407ac73fa5807bf42e5
lukasz.danelczyk <lukasz.danelczyk.sp@lhsystems.com>
2022-05-25 18:27:09 +0200

    Update spring cloud version, add spring cloud bus and clean up

--- src/main/java/com/lddelz/limitsservice/controller/LimitsController.java
+++ src/main/java/com/lddelz/limitsservice/controller/LimitsController.java
@@ -2,11 +2,17 @@ package com.lddelz.limitsservice.controller;
 
 import com.lddelz.limitsservice.configuration.LimitsProperties;
 import com.lddelz.limitsservice.domain.Limits;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class LimitsController {
+
+    private final Logger logger = LoggerFactory.getLogger(LimitsController.class);
 
     private final LimitsProperties limitsProperties;
 
@@ -16,6 +22,9 @@ public class LimitsController {
 
     @GetMapping("/limits")
     public Limits retrieveLimits() {
+
+        logger.info("Current limits: min {}, max {}", limitsProperties.getMinimum(), limitsProperties.getMaximum());
+
         return new Limits(limitsProperties.getMinimum(), limitsProperties.getMaximum());
     }
 
